d500489d798dab258f86de83912c59c8f5bb07a9
Erik Doernenburg <erik@doernenburg.com>
2013-05-15 12:41:49 +0200

    Made mapped attributes configurable.

--- public/draw-sizehistogram.js
+++ public/draw-sizehistogram.js
@@ -1,11 +1,17 @@
 $( document ).ready(function() {
 
-  data.sort(function(da, db) { return db.FLENGTH - da.FLENGTH} )
+	var at = {
+		SORT: "FLENGTH",
+		HEIGHT: "LOC",
+		SHADE: "WMC",
+	}
 
-	var CHEIGHT = 600
-  var BWIDTH = 8
+	var CHEIGHT = 600;
+  var BWIDTH = 8;
 	var BGAP = 2;
-	var BMINHEIGHT = 5
+	var BMINHEIGHT = 5;
+
+  data.sort(function(da, db) { return db[at.SORT] - da[at.SORT]} )
 
 	var div = d3.select("body").append("div")   
 	    .attr("class", "tooltip")               
@@ -17,11 +23,11 @@ $( document ).ready(function() {
     .attr("height", CHEIGHT + 1);
 		
   var yscale = d3.scale.linear()
-    .domain([0, d3.max(data, function(d) { return d.LOC })])
+    .domain([0, d3.max(data, function(d) { return d[at.HEIGHT] })])
     .range([0, CHEIGHT]);
 
   var fscale = d3.scale.linear()
-    .domain([0, d3.max(data, function(d) { return d.WMC })])
+    .domain([0, d3.max(data, function(d) { return d[at.SHADE] })])
     .range([100, 0]);
 
   chart.selectAll("line")
@@ -29,18 +35,18 @@ $( document ).ready(function() {
     .enter().append("line")
     .attr("x1", 0)
     .attr("x2", (BWIDTH + BGAP) * data.length)
-    .attr("y1", function(d) { return CHEIGHT - yscale(d) })
-    .attr("y2", function(d) { return CHEIGHT - yscale(d) })
+    .attr("y1", function(td) { return CHEIGHT - yscale(td) })
+    .attr("y2", function(td) { return CHEIGHT - yscale(td) })
     .style("stroke", "#ccc");
 
   chart.selectAll("rect")
     .data(data)
     .enter().append("rect")
     .attr("x", function(d, i) { return i * (BWIDTH + BGAP); })
-		.attr("y", function(d) { return CHEIGHT - yscale(d.LOC) })
-    .attr("height", function(d) { return Math.max(BMINHEIGHT, yscale(d.LOC)) })
+		.attr("y", function(d) { return CHEIGHT - Math.max(BMINHEIGHT, yscale(d[at.HEIGHT])) })
+    .attr("height", function(d) { return Math.max(BMINHEIGHT, yscale(d[at.HEIGHT])) })
     .attr("width", BWIDTH)
-    .style("fill", function(d) { return "hsl(200, 80%, " + fscale(d.WMC) + "%)" })
+    .style("fill", function(d) { return "hsl(200, 80%, " + fscale(d[at.SHADE]) + "%)" })
 		.on("mouseover", function(d) {      
 				div.transition()        
            .duration(200)      
